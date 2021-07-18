76f73c35e3f13d49266723e83e68d542dfa5f4e8
leo <liyinjia0452@gmail.com>
2021-07-18 20:19:48 +1200

    setup env

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e5e9aa000369db5f127bb15c64f5fdff'),
+    },
+  },
+});
