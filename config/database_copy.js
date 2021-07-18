b5f3e8051c3fa2a268e3ea90a524ce744e9cd4a3
leo <liyinjia0452@gmail.com>
2021-07-18 22:40:10 +1200

    add nav

--- config/database_copy.js
+++ config/database_copy.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'blog'),
-        username: env('DATABASE_USERNAME', 'blog'),
-        password: env('DATABASE_PASSWORD', '1'),
+        database: env('DATABASE_NAME', 'xxxx'),
+        username: env('DATABASE_USERNAME', 'xxxx'),
+        password: env('DATABASE_PASSWORD', 'xxxx'),
         ssl: env.bool('DATABASE_SSL', true),
       },
       options: {}
